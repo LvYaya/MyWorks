e93ecf49cbde137aad6fa625ae0b38eaa8c1d4e9
lvyaya <2547177455@qq.com>
2017-10-16 21:15:45 +0800

    first commit

---
+++ static/js/index.js
@@ -0,0 +1,17 @@
+$(function(){
+   
+
+   /*
+    $('#content').hide(); 
+    // $('form').removeAttr("onsubmit");  
+    $('.header-form').submit(function(e){
+      $('#header-img').hide();
+       $('#content').show();
+       $('.header-form').width('70%').css('margin-left','0');
+    });
+
+  */
+  
+  /*detail*/
+ // $('.content-left1').height($('.content-right1').height());
+})
